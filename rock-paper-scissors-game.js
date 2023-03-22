b8ddcf36f6a9686cb70e46fe68933a764d67ac26
ahorriblename <124475233+ahorriblename@users.noreply.github.com>
2023-03-21 21:32:04 -0700

    Create function that plays a round of roshambo

--- rock-paper-scissors-game.js
+++ rock-paper-scissors-game.js
@@ -1,5 +1,48 @@
-function getComputerChoice () {
-let choices = ['Rock', 'Paper', 'Scissors']
-let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-return computerChoice
+function getComputerChoice() {
+  let choices = ['Rock', 'Paper', 'Scissors']
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  return computerChoice
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase()
+  computerSelection = getComputerChoice();
+
+  switch (true) {
+    case playerSelection === 'rock' && computerSelection === 'Paper':
+      console.log('You lost! Paper beats rock!');
+      break;
+
+    case playerSelection === 'rock' && computerSelection === 'Rock':
+      console.log('Draw! You both picked rock!');
+      break;
+  
+    case playerSelection === 'rock' && computerSelection === 'Scissors':
+      console.log('You won! Scissors beats rock!');
+      break;
+
+    case playerSelection === 'paper' && computerSelection === 'Paper':
+      console.log('Draw! You both picked paper!');
+      break;
+
+    case playerSelection === 'paper' && computerSelection === 'Rock':
+      console.log('You won! Paper beats rock!');
+      break;
+
+    case playerSelection === 'paper' && computerSelection === 'Scissors':
+      console.log('You lost! Scissors beats paper!');
+      break;
+      
+    case playerSelection === 'scissors' && computerSelection === 'Paper':
+      console.log('You won! Scissors beats paper!');
+      break;
+
+    case playerSelection === 'scissors' && computerSelection === 'Rock':
+      console.log('You lost! Rock beats scissors!');
+      break;
+
+    case playerSelection === 'scissors' && computerSelection === 'Scissors':
+      console.log('Draw! You both picked scissors!');
+      break;
+  }
 }
